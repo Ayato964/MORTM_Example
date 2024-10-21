5ea03e52c98916d55f1b4f50d10db39ddf177ba0
YFKS <Takayuyq0517>
2024-10-21 11:52:16 +0900

    MORTMの使い方を解説

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from mortm import constants
 import torch
 from mortm.mortm import MORTM
-import pretty_midi as pm
 import mortm.tokenizer as token
 import numpy as np
 
@@ -11,33 +9,65 @@ from mortm.progress import _DefaultLearningProgress
 from mortm.tokenizer import get_token_converter
 from mortm.de_convert import ct_tokens_to_midi
 
+'''
+MORTMのバージョンは常に新しくなる為、モデルのバージョンとvocab_list.jsonを確認してください。
+うまくメロディが生成できない場合や、エラーが発生する場合、以下の項目を確認してください。
+
+1.model.load_state_dictでエラーが発生する。
+    -ハイパーパラメータが正しいか確認してください。モデルのバージョンによって、パラメータが異なる可能性があります。
+    -CPUを使っているか、GPUを使っているかを確認してください。
+    もし、CPUを使っている場合、 torch.load("model/ *** ", map_location="cpu")を設定してください。
+    
+2. 生成する時にエラーが発生する
+    - 配列構造が不正である可能性があります。サンプリングに入力する配列は1次元配列になるはずです。
+    
+3. 意味不明なメロディが生成される。
+    - 生成できたが、メロディとして成り立っていない場合、vocab_list.jsonが古い場合があります。
+    モデルによって異なるので、再度確認してください。
+'''
 
 tokenizer = token.Tokenizer(token=get_token_converter(120, TO_MUSIC), load_data="model/vocab/vocab_list.json")
 
-model = MORTM(
+model = MORTM( # MORTMのモデルのハイパーパラメータを設計
     progress=_DefaultLearningProgress(),
     vocab_size=327,
     position_length=8500,
     trans_layer=9, num_heads=32, d_model=1024,
     dim_feedforward=4096
 )
-model.load_state_dict(torch.load("model/MORTM.train.0.1.9445.pth"))
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.load_state_dict(torch.load("model/ *** ")) # モデルをロードする。
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # デバイスを設定
 model.to(device)
 
-# メロディ生成の実行
+'''
+既存の楽曲からその続きを生成する場合、以下を実行し、NPZから解凍してください。
+システム的な事情でarray1からメロディが記録されています。
+
+！MIDIから直接旋律を生成することはできません。！
+!実行する際はconvert.pyモジュールを使用し、MIDIをトークンのシーケンスに変換してください。!
+'''
+
 np_notes = np.load("ex/Test1.npz")
 
 start = np_notes[f'array1'][:50]
 
+'''
+一から、もしくはメロディをプログラマーが設定したい場合、以下を実行します。
+シーケンスは数値の配列です。tokenizerで文字列からトークンに変換してください。
+'''
 #start = [tokenizer.get(constants.START_SEQ_TOKEN)]
 
-print(f"First:{start}")
+print(f"First:{start}") # ロードしたシーケンスを表示
 
+'''
+シーケンスの生成は以下の2つから選べます。
+1. Top P sampling
+    -これは、確率の閾値Pを設定し、それ以上に該当するトークンからサンプリングを行います。複数存在する場合、ランダムでトークンを選びます。
+2. Top K sampling
+    - これは、確率の高い順番からK個のトークンを取得し、サンプリングを行います。複数存在する場合、ランダムでトークンを選びます。
+'''
 #gene = model.top_p_sampling(start, tokenizer, max_length=20, temperature=2.0)
 gene = model.top_k_sampling_with_temperature_sequence(start, max_length=500, temperature=1.1, top_k=5)
-
-#gene = model.generate_by_length(start, max_length=20)
 
 output = gene
 for t in output:
@@ -45,4 +75,4 @@ for t in output:
     print(f"{t}  {tokenizer.rev_get(t.tolist())}")
 
 
-midi = ct_tokens_to_midi(tokenizer, output, "out/generate_test.midi")
+midi = ct_tokens_to_midi(tokenizer, output, "out/generate_test.midi") #生成したトークンをMIDIに変換する。
